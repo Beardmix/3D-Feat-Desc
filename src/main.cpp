b54202033101c6c7182dd98cf67ed7f93b0c585e
mathieuLynch <mathieu.bulliot@gmail.com>
2013-05-14 16:20:57 +0100

    New main

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 	Engineering School
 */
 
-#include "defines.h"
+#include "KinectSearch/KinectSearch2_0.h"
 
 
 
@@ -23,24 +23,24 @@ main (int argc, char** argv)
 {   
   // Parameters
   int a, b, c, d, e, f;
-  char * str;
   if (argc > 1)
   {
-    str = argv[1]; a = atoi(argv[2]); b = atoi(argv[3]); c = atoi(argv[4]); d = atoi(argv[5]); e = atoi(argv[6]); f = atoi(argv[7]);
+    a = atoi(argv[1]); b = atoi(argv[2]); c = atoi(argv[3]); d = atoi(argv[4]); e = atoi(argv[5]); f = atoi(argv[6]);
   }
   else
   {
-  	str = (char *) "cloud11"; a = 5; b = 10; c = 10; d = 50; e = 1; f = 5;
+  	 a = 5; b = 10; c = 10; d = 50; e = 1; f = 5;
   }
   /* parameters 
   The array containing the 6 parameters (Number of Spheres/Cones/Slices / Radius of the biggest Sphere (mm) / order (0-1)) / Nb of bins in histograms
   */
   int parameters [6] = {a, b, c, d, e, f};
-  std::cout << "syntax: <Cloud Name> <Nb Spheres> <Nb Cones> <Nb Slices> <Radius Max> <Ordering by Cones> <Nb Bins>" << std::endl;
-  std::cout << "default: ./exe cloud11 5 10 10 50 1 5" << std::endl;
-  std::cout << "parameters:    "<< str <<" "<< a <<" "<< b <<" "<< c <<" "<< d <<" "<< e <<" "<< f << std::endl;
+  std::cout << "syntax: <Nb Spheres> <Nb Cones> <Nb Slices> <Radius Max> <Ordering by Cones> <Nb Bins>" << std::endl;
+  std::cout << "default: ./exe 5 10 10 50 1 5" << std::endl;
+  std::cout << "parameters:    "<< a <<" "<< b <<" "<< c <<" "<< d <<" "<< e <<" "<< f << std::endl;
 
-
+	KinectSearch v (parameters); 
+	v.run ();
 	
   return (0);
 }
